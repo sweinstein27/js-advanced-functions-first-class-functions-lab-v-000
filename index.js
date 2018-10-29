4d4e52cce9de7031816b1068e7a69de7a2d330bf
Scott Weinstein <sweinstein27@gmail.com>
2018-10-29 13:06:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 const returnFirstTwoDrivers = function (drivers) {
   return drivers.slice(0, 2);
 };
+
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(drivers.length-3, length-1);
+};
